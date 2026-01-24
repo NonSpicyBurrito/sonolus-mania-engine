3deeb5f05ac7935b7d6c59fa12ae23f4667d709a
NonSpicyBurrito <burritoyangster@gmail.com>
2026-01-24 15:06:22 +0800

    Updated packages

--- watch/src/engine/watchData/archetypes/Initialization.ts
+++ watch/src/engine/watchData/archetypes/Initialization.ts
@@ -148,7 +148,7 @@ export class Initialization extends Archetype {
             background: false,
         })
 
-        ui.progress.set({
+        ui.progress.bar.set({
             anchor: uiRect.rb,
             pivot: { x: 0, y: 0 },
             size: { x: uiRect.h, y: 0.15 * ui.configuration.progress.scale },
@@ -243,7 +243,7 @@ export class Initialization extends Archetype {
             background: false,
         })
 
-        ui.progress.set({
+        ui.progress.bar.set({
             anchor: uiRect.lb,
             pivot: { x: 0, y: 0 },
             size: { x: uiRect.w, y: 0.15 * ui.configuration.progress.scale },
